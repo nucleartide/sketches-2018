f8bf0e5a6528782f417b3f4ca3fc2d754d9a760b
Jason Tu <jasontu4@gmail.com>
2017-11-26 13:37:29 -0500

    Add `button` module, closes #3

---
+++ button.lua
@@ -0,0 +1,20 @@
+
+local Button = {
+  Left  = 0,
+  Right = 0,
+  Up    = 0,
+  Down  = 0,
+  O     = 0,
+  X     = 0,
+}
+
+-- Test update.
+function _update()
+end
+
+-- Test draw.
+function _draw()
+  cls()
+end
+
+return Color
