8c64d19a9e6247c15a1b3a8fc3f737a10ef3ba32
Jason Tu <jasontu4@gmail.com>
2017-11-26 16:08:23 -0500

    Add working `thicc` demo

--- thicc_test.lua
+++ thicc_test.lua
@@ -1,16 +1,11 @@
 
--- function _init()
---   local Color = require('color')
---   print = thicc(print, Color.Pink)
--- end
+Color = require('color')
+thicc = require('thicc')
+print = thicc(print, Color.Pink)
 
--- function _init()
-  -- local Color = require('color', { use_game_loop = false })
--- end
+function _update()
+end
 
--- function _update()
--- end
--- 
--- function _draw()
---   print('this text has a pink border')
--- end
+function _draw()
+  print('this text has a pink border')
+end
