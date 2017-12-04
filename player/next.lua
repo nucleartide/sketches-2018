6b4e5b24bd82173d4093fca64328ca3b61825bba
Jason Tu <jasontu4@gmail.com>
2017-12-03 19:21:22 -0500

    cleanup

--- player/next.lua
+++ player/next.lua
@@ -1,7 +1,10 @@
 
+--
 -- Transition to next state, setting some fields in the process.
 -- Assumes the existence of `data.at` and `data.state`.
-local function next(state, data)
+--
+
+return function(state, data)
   if data.state ~= state then
     data.at = 0
     data.state = state
@@ -9,7 +12,6 @@ local function next(state, data)
     data.at += 1
     data.at %= 30 -- prevent overflow
   end
+
   return state(data)
 end
-
-return next
